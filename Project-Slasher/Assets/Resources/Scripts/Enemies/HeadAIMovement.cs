a45920204089076a6b3b5c8d88a854a1aec79e50
TburtonX <75140404+TburtonX@users.noreply.github.com>
2022-05-23 20:32:01 -0700

    Gunbot Prototype made
    Shoots too much like a gatling gun and the nav mesh is off. But it does kill the player

--- Project-Slasher/Assets/Resources/Scripts/Enemies/HeadAIMovement.cs
+++ Project-Slasher/Assets/Resources/Scripts/Enemies/HeadAIMovement.cs
@@ -1,18 +1,114 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class HeadAIMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Transform player;
+    [SerializeField] private LayerMask whatIsPlayer;
+    private Vector3 turnPoint;
+    private bool turnPointSet = false;
+    private bool playerSpotted = false;
+
+    [SerializeField] private GameObject barrel;
+
+    [SerializeField] private float sightRange;
+
+    [SerializeField] private float scopeRange;
+
+    [SerializeField] private float reloadDelay;
+
+    [SerializeField] private float alertTime;
+
+    [SerializeField] private float lookSpeed;
+
+    [SerializeField] GameObject bullet;
+
+
+    
+    void Awake()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
-    // Update is called once per frame
+    public bool GetPlayerSpotted()
+    {
+        return playerSpotted;
+    }
+
     void Update()
     {
-        
+        if (!playerSpotted)
+        {
+            if (Physics.Raycast(barrel.transform.position, barrel.transform.position, scopeRange, whatIsPlayer))
+            {
+                Shoot();
+                playerSpotted = true;
+            }
+
+            if (Vector3.Distance(this.transform.position, player.position) <= sightRange)
+            {
+                playerSpotted = true;
+            }
+
+            else 
+            {
+                TurnRandom();
+            }
+        }
+
+        else
+        {
+            TurnLockOn();
+            Invoke("Shoot", reloadDelay);
+            Invoke("Reset", alertTime);
+        }
     }
+
+    void Shoot()
+    {
+        Instantiate(bullet, barrel.transform.position, Quaternion.identity);
+    }
+
+    void TurnLockOn()
+    {
+        turnPoint = player.position;
+        turnPointSet = true;
+        Vector3 lookPos = turnPoint - transform.position;
+        Quaternion rotation = Quaternion.LookRotation(lookPos);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, lookSpeed);
+    }
+
+    void TurnRandom()
+    {
+        if(!turnPointSet) 
+        {
+            float x = Random.Range(25f, 180f);
+            float y = Random.Range(25f, 180f);
+            float z = Random.Range(0f, 359f);
+            turnPoint = new Vector3(x,y,z);
+            turnPointSet = true;
+        }
+
+        else
+        {
+            Vector3 lookPos = turnPoint - transform.position;
+            Quaternion rotation = Quaternion.LookRotation(lookPos);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, lookSpeed);
+
+            if (transform.rotation == rotation)
+            {
+                turnPointSet = false;
+            }
+        }
+    }
+
+    void Reset()
+    {
+        playerSpotted = false;
+    }
+
+
+
 }
