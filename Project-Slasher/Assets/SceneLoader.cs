6b943f6304ebadaed3ca0f252ecbbd0f7499e2ad
Brandon Hillig <brandonhillig@gmail.com>
2022-06-02 01:03:24 -0700

    Adjusted Scene Loader to load core scene

--- Project-Slasher/Assets/SceneLoader.cs
+++ Project-Slasher/Assets/SceneLoader.cs
@@ -5,8 +5,36 @@ using UnityEngine;
 
 public class SceneLoader : MonoBehaviour
 {
-    void Start()
+    [SerializeField]
+    private string coreScene;
+
+    [SerializeField]
+    private string currentLevelScene;
+
+    [SerializeField]
+    private string nextLevelScene;
+
+    [SerializeField]
+    private bool isLevel;
+
+    void Awake()
     {
-        SceneManager.LoadScene("ParticleEffectScene", LoadSceneMode.Additive);
+        // If this SceneLoader object is in a level scene, load the core utilities
+        if(isLevel)
+        {
+            LoadCoreFunctionality();
+        }
+        // SceneManager.LoadScene("ParticleEffectScene", LoadSceneMode.Additive);
+    }
+
+    private void LoadCoreFunctionality()
+    {
+        SceneManager.LoadScene(coreScene, LoadSceneMode.Additive);
+    }
+
+    private void LoadNextLevel()
+    {
+        SceneManager.LoadScene(nextLevelScene, LoadSceneMode.Additive);
+        SceneManager.UnloadSceneAsync(currentLevelScene);
     }
 }
